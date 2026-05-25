ac6bcf27b2b9f836563abaf91b737ad830005f1b
StagnantW <anggajohanesthesman@gmail.com>
2026-05-25 14:19:10 +0700

    feat: pendaftaran rute web fitur pesan
    Menghubungkan seluruh fungsi MessageController ke dalam file web routing dan melindunginya menggunakan middleware autentikasi dasar.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/messages', [MessageController::class, 'getConversations'])->name('messages.getConversations');
+    Route::post('/messages/create', [MessageController::class, 'createConversation'])->name('messages.createConversation');
+    Route::get('/messages/{userId}', [MessageController::class, 'getMessages'])->name('messages.getMessages');
+    Route::post('/messages', [MessageController::class, 'sendMessage'])->name('messages.sendMessage');
+    Route::delete('/messages/conversation/{userId}', [MessageController::class, 'removeFullConversation'])->name('messages.removeFullConversation');
+    Route::delete('/messages/{messageId}', [MessageController::class, 'removeMessage'])->name('messages.removeMessage');
 });
