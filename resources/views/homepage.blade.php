16df94c5c52f2145ee92b72cdf62c6f5c03e6a6b
Louise <louisewilliam08@gmail.com>
2026-05-27 23:30:56 +0700

    feat: add homepage and friends page navigation

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Postify</title>
+</head>
+<body>
+
+    <h1>Welcome to Postify</h1>
+
+    <a href="/friends">
+        <button>
+            Friends
+        </button>
+    </a>
+    
+</body>
+</html>
