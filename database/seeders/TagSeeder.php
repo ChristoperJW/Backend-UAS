ba012e013f6104cef15f2bb2c29cd829ababfdf8
Michael <michaelrespatis@gmail.com>
2026-06-06 01:43:35 +0700

    Menambahkan Seeder untuk Fitur Tags

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tags = ['Study', 'Holiday', 'Weekend', 'Event', 'Food', 'Travel', 'Lifestyle', 'Health', 'Fitness', 'Technology', 'Education', 'Entertainment', 'Fashion', 'Business', 'Finance', 'Sports', 'Music', 'Art', 'Culture', 'Nature'];
+
+        foreach ($tags as $tag) { 
+            Tag::updateOrCreate(
+                ['name' => $tag]
+            );
+        }
+    }
+}
