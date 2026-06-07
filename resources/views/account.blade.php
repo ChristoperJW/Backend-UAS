aae60214db4d73e9ce6494b8fa7fa73236d27975
cjw <Christoper1177@gmail.com>
2026-06-07 12:03:44 +0700

    fix: fixing unresponsive change acc feature

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -5,6 +5,13 @@
     </head>
     <body>
         <h1>Account Settings</h1>
+        <form action="/account/update" method="GET">
+            @csrf
+            <button type="submit">
+                Update Account
+            </button>
+        </form>
+        <hr>
         <form action="/account/delete" method="POST" onsubmit="return confirm('Are you absolutely sure you want to delete your account? This cannot be undone!');">
             @csrf
             <button type="submit" style="background: red; color: white; padding: 10px; border-radius: 5px; cursor: pointer;">
